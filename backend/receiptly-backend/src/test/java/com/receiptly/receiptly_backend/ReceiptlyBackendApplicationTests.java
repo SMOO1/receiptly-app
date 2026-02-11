f2d5f243581cfa02b12b60d4e474890684239456
SMOO1 <bbruh188@gmail.com>
2026-02-10 23:45:38 -0500

    Initial project setup: basic backend + repo structure

---
+++ backend/receiptly-backend/src/test/java/com/receiptly/receiptly_backend/ReceiptlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receiptly.receiptly_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ docs/api-spec.md
@@ -0,0 +1,17 @@
+## Upload Receipt
+
+POST /api/receipts/upload
+
+Form Data:
+
+* file: image
+
+Response:
+{
+"id": "uuid",
+"vendor": "Walmart",
+"date": "2026-01-12",
+"total": 54.23,
+"imageUrl": "..."
+}
+
