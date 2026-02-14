bbd2d24d45d96f97eb67a46aea89b2740f2a7daf
SMOO1 <bbruh188@gmail.com>
2026-02-14 00:13:52 -0500

    version 1 of receiptController (making it possible to uplaod files)

---
+++ backend/src/main/java/com/controller/ReceiptController.java
@@ -0,0 +1,41 @@
+package com.receiptlyapp.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+
+@RestController
+@RequestMapping("/api/receipts")
+@CrossOrigin 
+public class ReceiptController {
+
+    private static final String UPLOAD_DIR = "uploads/";
+
+    @PostMapping("/upload")
+    public String uploadReceipt(@RequestParam("file") MultipartFile file) {
+        
+        if(file.isEmpty()){
+            return "No file uploaded"; 
+        }
+
+        try {
+            File directory = new File(UPLOAD_DIR);
+            if(!directory.exists()){
+                directory.mkdirs(); 
+            }
+
+            String filepath = UPLOAD_DIR + file.getOriginalFilename();
+            file.transferTo(new File(filepath)); 
+
+            return "File uploaded successfully"; 
+        }
+        catch(IOexception e){
+            e.printStackTrace();
+            return "Upload failed";
+        }
+
+    }
+}
--- backend/src/main/java/com/receiptly/receiptly_backend/ReceiptlyBackendApplication.java
+++ backend/src/main/java/com/receiptly/receiptly_backend/ReceiptlyBackendApplication.java
@@ -1,5 +1,6 @@
 package com.receiptly.receiptly_backend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
